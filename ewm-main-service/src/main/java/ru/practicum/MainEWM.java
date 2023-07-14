8e3ba705bc4ec866cb9f881f77197af0d99616b8
Oborin Nikita <oborinikita@yandex.ru>
2023-07-14 20:28:44 +0700

    диплом, модуль 2, на первую проверку

---
+++ ewm-main-service/src/main/java/ru/practicum/MainEWM.java
@@ -0,0 +1,11 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainEWM {
+    public static void main(String[] args) {
+        SpringApplication.run(MainEWM.class, args);
+    }
+}
---
+++ ewm-stats-service/stats-client/src/main/java/ru/practicum/StatClient.java
@@ -0,0 +1,43 @@
+package ru.practicum;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.client.BaseClient;
+import ru.practicum.modelDto.HitDto;
+import ru.practicum.modelDto.StatDto;
+import java.util.Map;
+
+@Service
+public class StatClient extends BaseClient {
+
+    private static final String API_PREFIX = "/hit";
+
+    @Autowired
+    public StatClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> createHit(HitDto hit) {
+        return post("/hit", hit);
+    }
+
+    public ResponseEntity<StatDto[]> getStat(String start, String end, String[] uris, boolean unique) {
+        Map<String, Object> parameters = Map.of(
+                "start", start,
+                "end", end,
+                "uris", uris,
+                "unique", unique
+        );
+        return getList("/stats?start={start}&end={end}&uris={uris}&unique={unique}", null, parameters);
+    }
+}
